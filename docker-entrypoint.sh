40608a6880406161bbbaf6216decc0d3443907b8
Bryan-Kirk Reinhardt <bkr@bkrs-MBP-15-work.local>
2020-05-29 13:18:02 -0400

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -22,5 +22,6 @@ mkdir -p -m 777 .kube
 # Render templates to config locations
 envtpl -o .aws/credentials -m error ./configmaps/aws-credentials/credentials
 envtpl -o .kube/config -m error ./configmaps/kubectl-config/config
+envtpl -o .k8ctl-server.yml -m error ./configmaps/k8ctl-server-config
 
 exec k8ctl-server start ${debug_flag}
